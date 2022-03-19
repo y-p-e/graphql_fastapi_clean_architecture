0c64677bd71fd7c97d4a00a3051e805ecb49d46b
Yuki Tanio <yuki_tanio@yahoo.co.jp>
2022-03-19 18:47:53 +0900

    graphql_query削除

--- main.py
+++ main.py
@@ -2,16 +2,34 @@ from fastapi import FastAPI
 from models import Base
 from database import engine
 from entities.user_entitiy import UserBaseModel
-from graphql_query import graphql_app
 from entities.user_entitiy import UserBaseModel
 from controllers.rest_controller import RestUserController
+import strawberry
+from strawberry.fastapi import GraphQLRouter
+from entities.user_entitiy import UserType
+from strawberry import ID
+from controllers.graphql_controller import GrapqlUserController
 
 
 Base.metadata.create_all(bind=engine)
 
+
+#GraphQL
+@strawberry.type
+class Query:
+    @strawberry.field
+    def get_user(self, user_id: ID) -> UserType:
+        return GrapqlUserController().get_user(user_id)
+
+schema = strawberry.Schema(Query)
+graphql_app = GraphQLRouter(schema)
+
+
 app = FastAPI()
 app.include_router(graphql_app, prefix="/graphql")
 
+
+#REST
 @app.get("/user/", response_model=UserBaseModel)
 def get_user(user_id: int):
     return RestUserController().get_user(user_id)
